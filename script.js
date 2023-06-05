8c231c98edb9763597f9be8c766edefe8d5f7d16
Qianyun Mu <msbluemu@gmail.com>
2023-06-05 14:31:13 +1000

    Initial commit

---
+++ script.js
@@ -0,0 +1,115 @@
+ const SEARCH_API = 'https://www.googleapis.com/books/v1/volumes?q=';
+ const API_KEY= '&key=AIzaSyCMluP6sEA55bAIKbuqJniMcANCE9w1-Qc';
+ const main = document.getElementById('main');
+ const search = document.getElementById('search-term');
+ const form = document.querySelector('form');
+ const authorRadio = document.getElementById('author');
+ const titleRadio = document.getElementById('tv');
+ const readingListLink = document.querySelector('.nav-link');
+
+async function fetchBook(url){
+    const res = await fetch(url)
+    const data = await res.json()
+    showBooks(data.items)
+}
+
+
+function addToReadingList(book) {
+  const readingList = JSON.parse(localStorage.getItem('readingList')) || [];
+  
+  // Check if the book is already in the reading list
+  const existingBook = readingList.find((item) => item.id === book.id);
+  if (existingBook) {
+    alert('This book is already in the reading list.');
+    return;
+  }
+  
+  readingList.push(book);
+  localStorage.setItem('readingList', JSON.stringify(readingList));
+}
+
+function showBooks(books, isReadingList){
+  main.innerHTML = ''
+  books.forEach((book) => {
+    const { title, authors, description, imageLinks, previewLink } = book.volumeInfo;
+
+    const imageSrc = imageLinks ? imageLinks.thumbnail : 'placeholder.jpg';
+
+    const bookElement = document.createElement('div');
+    bookElement.classList.add('book');
+
+    bookElement.innerHTML = `
+        <img src="${imageSrc}" alt="Book Cover">
+        <h2>${title}</h2>
+        <p>${authors && authors.length > 0 ? 'Author' + (authors.length > 1 ? 's' : '') + ': ' + authors.join(', ') : 'Author: Unknown'}</p>
+        <p class="description">${description}</p>
+        ${isReadingList ? `<button class="read-book">Read Book</button>` : `<button class="add-to-reading-list">Add to Reading List</button>`}
+    `;
+
+    if (isReadingList) {
+      const readBookButton = bookElement.querySelector('.read-book');
+      readBookButton.addEventListener('click', () => {
+        openBookPreview(previewLink);
+      });
+    } else {
+      const addToReadingListButton = bookElement.querySelector('.add-to-reading-list');
+      addToReadingListButton.addEventListener('click', () => {
+        addToReadingList(book);
+      });
+    }
+
+    main.appendChild(bookElement);
+  
+  });
+}
+
+function displayReadingList() {
+  const readingList = JSON.parse(localStorage.getItem('readingList')) || [];
+  showBooks(readingList, true);
+}
+
+readingListLink.addEventListener('click', () => {
+  displayReadingList();
+});
+
+function openBookPreview(previewLink) {
+  window.open(previewLink, '_blank');
+}
+
+form.addEventListener('submit', (e)=> {
+  e.preventDefault()
+  const searchTerm = search.value;
+  let searchType = 'inauthor';
+  if (authorRadio.checked) {
+    searchType = 'inauthor';
+  } else if (titleRadio.checked) {
+    searchType = 'intitle';
+  }
+
+  if(searchTerm && searchTerm !== ''){
+    const url = SEARCH_API + searchType + ':' + searchTerm + API_KEY;
+    fetchBook(url);
+    search.value = '';
+  } else {
+    window.location.reload()
+  }
+}) 
+
+
+// async function display() {
+//     try {
+//       const results = await fetchAPIData('volumes');
+//       console.log(results);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }
+
+
+
+//search authors
+// async function search(){
+//     const queryString = window.location.search;
+//     const urlParams = new URLSearchParams(queryString);
+//     console.log(urlParams);
+// }
